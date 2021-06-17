32fd49257185c4659f965a0a3667ad4502a42b41
DemoJameson <demojameson@gmail.com>
2021-06-17 17:58:42 +0800

    code cleanup

--- Patches/PlayMakerUnity2DProxy.cs
+++ Patches/PlayMakerUnity2DProxy.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using Assembly_CSharp.TasInfo.mm.Source;
-using Modding;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using MonoMod;
+using MonoMod.Cil;
 using MonoMod.Utils;
 
 // ReSharper disable All
@@ -20,19 +20,17 @@ public class patch_PlayMakerUnity2DProxy : PlayMakerUnity2DProxy {
     }
 }
 
-namespace Modding {
+namespace MonoMod {
     [MonoModCustomAttribute(nameof(MonoModRules.PatchStart))]
     internal class PatchStartAttribute : Attribute { }
-}
 
-namespace MonoMod {
     static partial class MonoModRules {
         public static void PatchStart(MethodDefinition method, CustomAttribute attrib) {
             MethodDefinition methodDefinition = method.DeclaringType.FindMethod("System.Void OnColliderCreate()");
-            
+
             Mono.Collections.Generic.Collection<Instruction> instrs = method.Body.Instructions;
             ILProcessor il = method.Body.GetILProcessor();
-            
+
             instrs.Insert(0, il.Create(OpCodes.Ldarg_0));
             instrs.Insert(1, il.Create(OpCodes.Call, methodDefinition));
         }
